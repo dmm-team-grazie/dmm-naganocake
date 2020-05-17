74b7b40c4bd86a7f32a6aee9485d372982541421
vagrant <vagrant@localhost.localdomain>
2020-05-17 05:23:41 +0000

    first commit

---
+++ db/migrate/20200517032030_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.integer "genre_id",        null: false
+      t.string "name",             null: false
+      t.integer "non_taxed_price", null: false
+      t.text "introduction",       null: false
+      t.string "item_image_id",    null: false
+      t.boolean "is_valid",        null: false, default: "TRUE"
+      t.timestamps
+    end
+    add_index :items, :id
+  end
+end
