4e323323e297721188d0b81416e670c4a286f5aa
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-05-18 09:38:00 +0000

    [update]devise.rbファイル変更

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,17 @@
 class CartItemsController < ApplicationController
+	def index
+		@cart_item = CartItem.all
+	end
+
+	def create
+	end
+
+	def destroy_all
+	end
+
+	def update
+	end
+
+	def destroy_each
+	end
 end
