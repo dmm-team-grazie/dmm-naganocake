946b669e8eac27ccfe3a4d3e92b6356180d09d00
Ben-1327 <ff.walker1327@gmail.com>
2020-05-19 02:16:54 +0000

    [Update] Bootstrap

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,6 +2,11 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.page(params[:page]).reverse_order
+    @genres = Genre.all
+  end
+
+  def show
+    @genres = Genre.all
   end
 
 end
