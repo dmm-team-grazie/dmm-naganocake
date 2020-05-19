e4f67242541ac48ce6e82fe5f723bcba4ffa96c6
yuhiusui <usui.hanshin@gmail.com>
2020-05-19 02:16:04 +0000

    update order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,16 +1,21 @@
 class Public::OrdersController < ApplicationController
   def thanks 
   end
-
+  def new
+    # @neworder = Order.new
+  end
   def index
     # @orders = Order.all 
   end
   def show 
     # @order = Order.find(params[:id])
   end
-  def input 
+  def input
+    # @neworder = Order.new
   end
   def confirm 
+    @order = Order.new(order_params)
+    render :new if @order.invalid?
   end
   
 
