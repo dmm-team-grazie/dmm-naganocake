90367dc0d7215f70698140dcfd79de7477d96554
yuhiusui <usui.hanshin@gmail.com>
2020-05-19 08:17:41 +0000

    [update]views, routes, model(hasmany, 仮enum)

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,10 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
   def show
+    @order = Order.find(params[:id])
+    @items = Order.items.all
   end
   def update
   end
