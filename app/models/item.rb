0061ae5a89a7dac29e4ea7c1e6d80e163bc79465
Ben-1327 <ff.walker1327@gmail.com>
2020-05-19 09:11:36 +0000

    [Update] some view files of item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,9 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   belongs_to :genre
+
+  def taxed_price
+    tax = 1.1
+    return ((self.non_taxed_price * tax).round(2)).ceil
+  end
 end
