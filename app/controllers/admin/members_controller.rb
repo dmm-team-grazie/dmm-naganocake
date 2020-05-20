240b6d2b85cdc5924838dbbd94a576b84642bc9c
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-05-20 01:08:18 +0000

    [add]admin/member

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,2 +1,12 @@
-class MembersController < ApplicationController
+class Admin::MembersController < ApplicationController
+	def index
+		@members = Member.all
+	end
+
+	def show
+		@member = Member.find(params[:id])
+	end
+
+	def edit
+	end
 end
--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -2,6 +2,14 @@ class Public::MembersController < ApplicationController
 	def show
 	end
 
+	def edit
+	end
+
+	def update
+	end
+
 	def leave
 	end
+
+
 end
