ec47e6afed73d03ee7cbcb135522ad4822484c11
Ben-1327 <ff.walker1327@gmail.com>
2020-05-21 04:53:32 +0000

    [Fix] routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,32 +4,30 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :public do
-    resources :members, only:[:create, :update]
     get '' => 'members#show'
     get 'edit' => 'members#edit'
     get 'leave' => 'members#leave'
-    resources :items, except:[:destroy]
     delete 'cart_items' => 'cart_items#destroy_all'
     delete 'cart_items/:id' => 'cart_items#destroy_each'
-    resources :addresses
-    resources :orders
     get 'orders/thanks' => 'orders#thanks'
     post 'orders/confirm' => 'orders#confirm'
-
+    resources :members, only:[:create, :update]
+    resources :items, except:[:destroy]
+    resources :addresses
+    resources :orders
+    resources :cart_items, only: [:index, :create, :update]
   end
 
   root 'homes#top'
 
   namespace :admin do
-    resources :items, except:[:new]
+    get 'top' => 'admins#top'
     get 'items/:id/new' => 'items#new'
+    resources :items, except:[:new]
     resources :members
     resources :orders
     resources :genres
     resources :order_details, only: [:update]
-
-
-    get 'top' => 'admins#top'
   end
 
 end
