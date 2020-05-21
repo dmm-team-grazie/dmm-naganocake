a6fd22ad5f9771fc95b0a1aa106148332a68b91e
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-05-21 02:14:18 +0000

    [update]editファイル

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -11,5 +11,10 @@ class Public::MembersController < ApplicationController
 	def leave
 	end
 
+	private
+	def member_params
+		params.require(:member).permit(:last_name,:first_name,:last_kana_name,:first_kana_name,:email,:postal_code,:address,:phone_number)
+	end
+
 
 end
