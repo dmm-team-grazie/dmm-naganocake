7b52d5c1662f44c63d30906501eefceac507f79b
Ben-1327 <ff.walker1327@gmail.com>
2020-05-21 02:12:19 +0000

    [Add] Address files

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,4 +14,32 @@ class Admin::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def create
+    @item = Item.new(item_params)
+  	if @item.save
+  		redirect_to admin_item_path(@item), notice: "successfully created item!"#保存された場合の移動先を指定。
+  	else
+  		@items = Item.all.page(params[:page]).reverse_order.per(10)
+  		render :index
+  	end
+
+    def update
+    	@item = Item.find(params[:id])
+    	if @item.update(item_params)
+    		redirect_to admin_item_path(@item), notice: "successfully updated item!"
+    	else 
+    		render :edit
+    	end
+    end
+
+
+  end
+
+  private
+
+  def  item_params
+  	params.require(:item).permit(:genre_id,:name,:non_taxed_price,:introduction,:item_image_id,:is_valid)
+  end
+
 end
