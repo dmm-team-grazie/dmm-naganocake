41da344e4186f955935d114925c9f4abe1086fdb
odayutaka <y.oda.sanwadock@gmail.com>
2020-05-21 02:12:56 +0000

    [update]ジャンル一覧view編集

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,10 +1,33 @@
-class GenresController < ApplicationController
+class Admin::GenresController < ApplicationController
 	def index
+		@genres = Genre.all
+		@genre = Genre.new
 	end
-	def creare
-	end
+
+	def create
+		@genre = Genre.new(genre_params)
+		if @genre.save
+    	redirect_to admin_genres_path, notice: "ジャンルの登録に成功しました."
+    else
+      render 'index'
+    end
+  end
+
 	def edit
+		@genre = Genre.find(params[:id])
 	end
+
 	def update
+		@genre = Genre.find(params[:id])
+		if @genre.update(genre_params)
+			redirect_to admin_genres_path, notice: "ジャンルの編集に成功しました。"
+		else
+      render 'edit'
+    end
+	end
+
+	private
+	def genre_params
+		params.require(:genre).permit(:name, :is_valid)
 	end
 end
