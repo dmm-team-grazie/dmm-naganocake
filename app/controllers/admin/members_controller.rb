4dd6bec5c99fce1a65f847bcc7f476e1cdd916c6
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-05-21 07:25:11 +0000

    [update]admin/member 会員ステータス記述

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,6 +1,6 @@
 class Admin::MembersController < ApplicationController
 	def index
-		# @members = Member.page(params[:page]).reverse_order
+		@members = Member.page(params[:page]).reverse_order
 	end
 
 	def show
@@ -9,5 +9,16 @@ class Admin::MembersController < ApplicationController
 
 	def edit
 		@member = Member.find(params[:id])
+	end
+
+	def update
+		@member = Member.find(params[:id])
+		@member.update(member_params)
+		redirect_to admin_member_path, notice: "会員情報の編集に成功しました。"
+	end
+
+	private
+	def member_params
+		params.require(:member).permit(:last_name,:first_name,:last_kana_name,:first_kana_name,:email,:postal_code,:address,:phone_number,:is_valid)
 	end
 end
