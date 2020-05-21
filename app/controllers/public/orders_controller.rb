6f5821996dd2bce2935afe61ca246459877837cb
yuhiusui <usui.hanshin@gmail.com>
2020-05-21 02:27:32 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,16 +5,17 @@ class Public::OrdersController < ApplicationController
     # @neworder = Order.new
   end
   def index
-    @orders = Order.all 
+    @orders = Order.page(params[:page]).reverse_order.per(5)
   end
   def show 
-    @order = Order.find(params[:id])
-    @items = Order.items.all
+    # @order = Order.find(params[:id])
+    @orders = Order.all
   end
   def input
     # @neworder = Order.new
   end
   def confirm 
+    @orders = Order.all
     @order = Order.new(order_params)
     render :new if @order.invalid?
   end
