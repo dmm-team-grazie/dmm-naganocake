93545d19a666acf64d9776435cb36050b6a23019
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-05-22 02:42:04 +0000

    [add]controllers/application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def taxed_price
+    tax = 1.1
+    return ((self.non_taxed_price * tax).round(2)).ceil
+  end
+  
   protected
   def after_sign_in_path_for(resource)
     case resource
