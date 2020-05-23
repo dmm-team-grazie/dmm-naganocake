959a504857b0f0d1bbbe553f12e4c677e4f465e8
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-05-23 07:53:10 +0000

    [fix]public/members show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :members, :path => "public/members"
+  # devise_for :members, :controllers => {:passwords => 'public/members/passwords'}
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
