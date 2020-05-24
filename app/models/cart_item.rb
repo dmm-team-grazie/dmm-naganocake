7c5c05f6b38879b59e7312cb26122c6137bd126c
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-05-24 07:17:37 +0000

    [fix]cartitem/index

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -8,12 +8,4 @@ class CartItem < ApplicationRecord
     return ((self.non_taxed_price * tax).round(2)).ceil
   end
 
-  def total_price_from_cartitem
-    self.each do |cart_item|
-      item = cart_item.item
-      @subtotal_price += item.price * cart_item.number
-      @total_price = @subtotal_price
-    end
-  end
-
 end
