cf335c86dbba7bb80d213c8bcffc5713acfcab43
Ben-1327 <ff.walker1327@gmail.com>
2020-05-24 08:57:55 +0000

    [Fix] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :members, path: “public/members”, controllers:{
-    registrations: ‘members/registrations’,
-    sessions: ‘members/sessions’,
-    passwords: ‘members/passwords’
+    devise_for :members, path: "public/members", controllers:{
+    registrations: 'members/registrations',
+    sessions: 'members/sessions',
+    passwords: 'members/passwords'
 }
-  # devise_for :members, :controllers => {:passwords => ‘public/members/passwords’}
+  # devise_for :members, :controllers => {:passwords => 'public/members/passwords'}
   devise_for :admins, controllers:{
-    registrations: ‘admins/registrations’,
-    sessions: ‘admins/sessions’,
-    passwords: ‘admins/passwords’
+    registrations: 'admins/registrations',
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords'
 }
   namespace :public do
-    get ‘’ => ‘members#show’
-    get ‘edit’ => ‘members#edit’
-    patch ‘’ => ‘members#update’
-    get ‘leave’ => ‘members#leave’
-    get ‘cart_items’ => ‘cart_items#index’
-    delete ‘cart_items’ => ‘cart_items#destroy_all’, as: ‘destroy_public_cart_items’
-    delete ‘cart_items/:id’ => ‘cart_items#destroy_each’,as: ‘destroy_public_cart_item’
-    get ‘orders/thanks’ => ‘orders#thanks’
-    post ‘orders/confirm’ => ‘orders#confirm’
+    get '' => 'members#show'
+    get 'edit' => 'members#edit'
+    patch '' => 'members#update'
+    get 'leave' => 'members#leave'
+    get 'cart_items' => 'cart_items#index'
+    delete 'cart_items' => 'cart_items#destroy_all', as: 'destroy_public_cart_items'
+    delete 'cart_items/:id' => 'cart_items#destroy_each',as: 'destroy_public_cart_item'
+    get 'orders/thanks' => 'orders#thanks'
+    post 'orders/confirm' => 'orders#confirm'
     resource :members, only:[:create]
     resources :items, except:[:destroy]
     resources :addresses
@@ -27,10 +27,10 @@ Rails.application.routes.draw do
     resources :cart_items, only: [:create, :update]
     resources :genres, only:[:show]
   end
-  root ‘public/homes#top’
+  root 'public/homes#top'
   namespace :admin do
-    get ‘top’ => ‘admins#top’
-    get ‘items/new’ => ‘items#new’
+    get 'top' => 'admins#top'
+    get 'items/new' => 'items#new'
     resources :items, except:[:new]
     resources :members
     resources :orders
